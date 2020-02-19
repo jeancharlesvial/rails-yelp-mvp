7d5b870dd9e925f8155a799b9ad68489916fc83e
Jeancharles Vial <jean-charles_vial@hotmail.fr>
2020-02-19 17:00:46 +0100

    Yelp MVP done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :content, uniqueness: true, presence: true
-  validates :rating, uniqueness: true, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5], message: 'Sorry, you have to choice between 0 and 5' }
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5], message: 'Sorry, you have to choice between 0 and 5' }
   validates_numericality_of :rating
 end
